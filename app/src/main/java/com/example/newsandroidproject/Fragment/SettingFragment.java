f92d2872db43c9a85c3348d3a9553a4f12b1b148
Kaito1820 <nguyenduykhanh1820@gmail.com>
2024-04-28 15:33:42 +0700

    add SettingPage UI

--- app/src/main/java/com/example/newsandroidproject/Fragment/SettingFragment.java
+++ app/src/main/java/com/example/newsandroidproject/Fragment/SettingFragment.java
@@ -1,15 +1,28 @@
 package com.example.newsandroidproject.Fragment;
 
+import android.animation.LayoutTransition;
+import android.content.Context;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.newsandroidproject.MainActivity;
 import com.example.newsandroidproject.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +39,7 @@ public class SettingFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    View rootView;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -61,16 +75,47 @@ public class SettingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
-        Button viewHistoryButton = rootView.findViewById(R.id.button);
-
-        viewHistoryButton.setOnClickListener(new View.OnClickListener() {
+        rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+        LinearLayout layoutSettingPage = rootView.findViewById(R.id.layoutSettingPage);
+        ConstraintLayout csUserSetting = rootView.findViewById(R.id.csUserSetting);
+        CardView userSetting = rootView.findViewById(R.id.crdvUserSetting);
+        CardView historySetting = rootView.findViewById(R.id.crdvHistory);
+        CardView helpSetting = rootView.findViewById(R.id.crdvHelp);
+        CardView aboutSetting = rootView.findViewById(R.id.crdvAbout);
+        CardView logout = rootView.findViewById(R.id.crdvLogout);
+        ListView detailsUserSetting = rootView.findViewById(R.id.detailsUserSetting);
+        layoutSettingPage.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+        csUserSetting.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+        userSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expandUserSetting(csUserSetting, detailsUserSetting);
+            }
+        });
+        historySetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openHistoryFragment();
             }
         });
+        helpSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+        aboutSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return rootView;
     }
 
@@ -79,5 +124,21 @@ public class SettingFragment extends Fragment {
         if (mainActivity != null) {
             mainActivity.openHistoryFragment();
         }
+    }
+
+    private void expandUserSetting(ConstraintLayout csUserSetting, ListView detailsUserSetting){
+        String data[] = {"Thông tin cá nhân", "Thay đổi mật khẩu"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);
+        detailsUserSetting.setAdapter(adapter);
+        int setList = (detailsUserSetting.getVisibility() == View.GONE) ? View.VISIBLE : View.GONE;
+        TransitionManager.beginDelayedTransition(csUserSetting, new AutoTransition());
+        detailsUserSetting.setVisibility(setList);
+
+        detailsUserSetting.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?>arg0, View arg1, int arg2, long arg3) {
+                        System.out.println("Test click on details user setting: " + data[arg2]);
+                    }
+                });
     }
 }
