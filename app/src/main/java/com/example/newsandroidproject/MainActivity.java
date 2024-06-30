8edd1c49e858a31ff657de37a2839e0fee7caa93
truonggiahuy8423 <zhuy8423@gmail.com>
2024-07-01 02:07:16 +0700

    Revert "Merge remote-tracking branch 'origin/Khanh_SignUp'"
    This reverts commit 95665393f7971eec283dd26bf97bedcb62cdc867, reversing
    changes made to 2a25836297f4b024586e6653489ace15564da1e5.

--- app/src/main/java/com/example/newsandroidproject/MainActivity.java
+++ app/src/main/java/com/example/newsandroidproject/MainActivity.java
@@ -119,8 +119,6 @@ public class MainActivity extends AppCompatActivity implements ArticleRecycleVie
 
         test();
 
-        setUpNavigationMenu();
-
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.home_page) {
                 if (!selectedFragmentIndex) {
